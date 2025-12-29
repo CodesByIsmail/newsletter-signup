a492e499c33ec2ffc99b8b57e82668f13d0a2354
[CodesByIsmail] <ismailolakunle1401@gmail.com>
2025-12-29 05:53:32 +0100

    up

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = queryselector('form');
+const submitBtn = queryselector('.submit__btn');
+const emailInput = queryselector('.email__input')
+
+form.addEventListener('submit', (e) => {
+    
+})
