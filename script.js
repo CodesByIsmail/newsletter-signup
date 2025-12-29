a3f2c3e37163b4a221a58f940b9a9acfe29fa052
CodesByIsmail <ismailolakunle1401@gmail.com>
2025-12-29 09:51:15 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const form = queryselector('form');
-const submitBtn = queryselector('.submit__btn');
-const emailInput = queryselector('.email__input')
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('.submit__btn');
+const emailInput = document.querySelector('.email__input')
+const errorMessage = document.querySelector('.error__message')
+
+// form.addEventListener('submit', (e) => {
+  
+// if (emailInput === '' || !emailInput.includes('@')) {
+//   e.preventDefault()
+//   errorMessage.innerText = 'Valid Email is required'
+// }
+// })
 
 form.addEventListener('submit', (e) => {
-    
+if (emailInput.value === '' || ![...emailInput.value].includes('@')) {
+   e.preventDefault()
+   errorMessage.textContent = 'Valid Email is required'
+ }
 })
